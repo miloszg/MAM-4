a7c5c801fc030f9d218a47cf7dadb63d4463f9f1
miloszg <milosz.gus@gmail.com>
2020-10-31 15:57:07 +0100

    final

--- app/src/main/java/pl/milosz/mediaplayer/MainActivity.java
+++ app/src/main/java/pl/milosz/mediaplayer/MainActivity.java
@@ -1,25 +1,32 @@
 package pl.milosz.mediaplayer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.DocumentsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
     MediaRecorder recorder;
     SeekBar musicBar;
     String path="";
-    TextView startTime, endTime, musicTextName;
-    Button deathGripsButton, recordedSoundButton, startRecordButton, stopRecordButton;
+    TextView startTime, endTime, musicTextName, directoryText;
+    Button deathGripsButton, recordedSoundButton, startRecordButton, stopRecordButton, directoryPickerButton;
+    EditText filenameText;
+    Intent fileIntent;
     int musicId=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         recordedSoundButton = findViewById(R.id.recordedSoundButton);
         startRecordButton = findViewById(R.id.startRecordButton);
         stopRecordButton = findViewById(R.id.stopRecordButton);
+        directoryPickerButton = findViewById(R.id.directoryPickerButton);
+        filenameText = findViewById(R.id.filenameText);
+        directoryText = findViewById(R.id.directoryText);
         deathGripsButton = findViewById(R.id.deathGripsButton);
         deathGripsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,13 +70,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // disable
+        stopRecordButton.setEnabled(false);
+        directoryPickerButton.setEnabled(false);
+        startRecordButton.setEnabled(false);
+        stopRecordButton.setEnabled(false);
+
+
         if(checkSelfPermission()){
+            directoryPickerButton.setEnabled(true);
+            directoryPickerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    fileIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE, null);
+                    fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    fileIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                    startActivityForResult(Intent.createChooser(fileIntent, "Choose directory"), 10);
+                }
+            });
+            startRecordButton.setEnabled(true);
             startRecordButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/"+ UUID.randomUUID().toString()+"_audio.3gp";
+                    String filename = filenameText.getText().toString();
+                    String directory = directoryText.getText().toString();
+                    Toast.makeText(MainActivity.this, directory+"/"+filename, Toast.LENGTH_SHORT).show();
+                    if(filename!=null && directory!=null) {
+                        path = directory + "/" + filename + "_" + UUID.randomUUID().toString() + ".3gp";
+                    } else {
+                        Toast.makeText(MainActivity.this, "Nie wybrano ścieżki i nazwy pliku, więc wybrane zostana automatycznie", Toast.LENGTH_LONG).show();
+                        path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + UUID.randomUUID().toString() + "_audio.3gp";
+                    }
+                    Toast.makeText(MainActivity.this, path, Toast.LENGTH_SHORT).show();
                     setupMediaRecorder();
-                    try{
+                    try {
                         recorder.prepare();
                         recorder.start();
                     } catch (IOException e) {
@@ -72,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     Toast.makeText(MainActivity.this, "Rozpoczęcie nagrywania...", Toast.LENGTH_SHORT).show();
                     startRecordButton.setEnabled(false);
+                    stopRecordButton.setEnabled(true);
                 }
             });
             stopRecordButton.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     recorder.stop();
                     startRecordButton.setEnabled(true);
+                    stopRecordButton.setEnabled(false);
                 }
             });
         } else {
@@ -100,6 +141,23 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.RECORD_AUDIO
         }, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK) {
+                    if(data.getData()!=null && data.getData().getPath()!=null){
+                        Uri treeUri = data.getData();
+                        String path = FileUtil.getFullPathFromTreeUri(treeUri, this);
+                        Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+                        directoryText.setText(path);
+                    }
+                }
+                break;
+        }
     }
 
     @Override
@@ -126,8 +184,9 @@ public class MainActivity extends AppCompatActivity {
         if(player == null){
             if(musicId == 0){
                 player = MediaPlayer.create(this, R.raw.deathgrips);
+                Toast.makeText(this, "Puszczono: Death Grips - Guillotine", Toast.LENGTH_SHORT).show();
             } else {
-                if(path.length()>0){
+                if(path!=null){
                     try {
                         player = new MediaPlayer();
                         player.setDataSource(path);
